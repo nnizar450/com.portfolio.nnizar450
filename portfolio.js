5ef1688debd6650d15c7935d5e33d3eadd6fbd19
Mohamed Nizar <149287639+nnizar450@users.noreply.github.com>
2024-09-21 21:05:45 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+let btn=document.getElementById("btn1");
+btn.addEventListener("click",()=>{
+    console.log("kk ")
+    window.location="https://drive.google.com/file/d/1Hqu25dWKTJsjRh4CXQO4pSvxtGFO4jg6/view?usp=drive_link";   
+})
+
